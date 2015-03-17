f368282ab5797daf4784eba435c2e3a6ea5b0039
Adrien Bouttier <hibigbob@gmail.com>
2015-03-17 21:35:05 +0100

    Migration express 3 to express 4

--- request/app.js
+++ request/app.js
@@ -1,60 +1,66 @@
 var express = require('express');
-var path = require('path');
+var routes = require('./routes/index.js');
+var user = require('./routes/users.js');
+var compression = require('compression');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var http = require('http');
+var path = require('path');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var methodOverride = require('method-override');
+var errorhandler = require('errorhandler');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(morgan('dev')); 
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json())    // parse application/json
+app.use(methodOverride()); 
+// app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorhandler())
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+
+app.get('/search', function(req, res) {
+  console.log(req.query);
+  res.end(JSON.stringify(req.query)+'\n');
 });
 
+app.get('/params/:role/:name/:status', function(req, res) {
+  console.log(req.params);
+  console.log(req.route);
+  res.end();
+});
 
-module.exports = app;
+app.post('/body', function(req, res){
+  console.log(req.body);
+  res.end(JSON.stringify(req.body)+'\n');
+});
+
+app.post('/upload', function(req, res){
+  console.log(req.files.archive);
+  //read req.files.archive.path
+  //process the data
+  //save the data
+  res.end();
+});
+
+app.get('/route', function(req, res){
+  console.log(req.route);
+  res.end();
+});
+app.use('/', routes);
+app.use('/users', user);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
